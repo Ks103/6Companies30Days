2ebd59bd1172f3f1320a00508e0a0b30131cb660
Khushi Shroff <kkhushi3058@gmail.com>
2023-01-11 21:29:47 +0530

    Day 1

---
+++ Adobe/1.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def fractionToDecimal(self, numerator, denominator):
+        """
+        :type numerator: int
+        :type denominator: int
+        :rtype: str
+        """
+        sign = '-' if numerator * denominator < 0 else ''
+        quotient, remainder = divmod(abs(numerator), abs(denominator))
+        result_list = [sign, str(quotient), '.']
+        remainders = []
+        while remainder not in remainders:
+            remainders.append(remainder)
+            quotient, remainder = divmod(remainder * 10, abs(denominator))
+            result_list.append(str(quotient))
+
+        idx = remainders.index(remainder)
+        result_list.insert(idx + 3, '(')
+        result_list.append(')')
+        result = ''.join(result_list).replace('(0)', '').rstrip('.')
+        return result
+
+
+if __name__ == "__main__":
+    assert Solution().fractionToDecimal(1, 2) == '0.5'
+    assert Solution().fractionToDecimal(2, 1) == '2'
+    assert Solution().fractionToDecimal(2, 3) == '0.(6)'
